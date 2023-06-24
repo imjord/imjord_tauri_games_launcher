6a3defffc149f7ad3d14c8e4ca7380f130bfd89a
imjord <dean.jay.goat@gmail.com>
2023-06-23 20:45:02 -0600

    have rust commands to add games/remove from game_paths file. having weird behaviour when adding/removing games the app closes and reopens.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,54 +4,108 @@ use rfd::FileDialog;
 use tauri::CustomMenuItem;
 use tauri::Manager;
 use std::process::Command;
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::Write;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// Define the name of the text file that will store the paths of the games
+const GAME_PATH_STORAGE: &str = "game_paths.txt";
+
+// A function that reads the game paths from the text file and returns them as a string
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_games() -> Result<String, String> {
+    // Attempt to read the contents of the game paths file
+    let contents = fs::read_to_string(GAME_PATH_STORAGE)
+        .map_err(|e| format!("Failed to read from file: {}", e))?;
+    // Return the contents of the file
+    Ok(contents)
 }
 
+// A function that opens a file dialog for the user to select a game executable,
+// then adds the path of that executable to the game paths file
 #[tauri::command]
 fn get_file_path() -> Result<String, String> {
+    // Create a new file dialog that filters for .exe files only
     let dialog = FileDialog::new().add_filter("Exe Files", &["exe"]);
 
-    match dialog.pick_file(){
-        Some(file_path) => Ok(file_path.to_string_lossy().into_owned()),
+    // Wait for the user to pick a file
+    match dialog.pick_file() {
+        // If the user picked a file
+        Some(file_path) => {
+            // Convert the file path to a string
+            let file_path_str = file_path
+                .to_str()
+                .ok_or("Failed to convert path to string")?;
+            // Open the game paths file in append mode
+            let mut file = OpenOptions::new()
+                .write(true)
+                .append(true)
+                .open(GAME_PATH_STORAGE)
+                .map_err(|e| format!("Failed to open file: {}", e))?;
+            // Write the path of the new game to the file
+            writeln!(file, "{}", file_path_str)
+                .map_err(|e| format!("Failed to write to file: {}", e))?;
+            // Return the path of the new game.
+            Ok(file_path_str.to_string())
+        },
+        // If the user didn't pick a file, return an error
         None => Err("No file selected".to_string()),
     }
 }
 
+// A function that removes a game from the game paths file.
+#[tauri::command]
+fn remove_game(game_name: String) -> Result<String, String> {
+    // Attempt to read the contents of the game paths file
+    let contents = fs::read_to_string(GAME_PATH_STORAGE)
+        .map_err(|e| format!("Failed to read from file: {}", e))?;
+
+    // Split the contents into lines
+    let lines: Vec<String> = contents.lines().map(String::from).collect();
+
+    // Filter out the line that matches the game to remove
+    let remaining_games: Vec<String> = lines
+        .into_iter()
+        .filter(|line| !line.contains(&game_name))
+        .collect();
+
+    // Write the remaining game paths back to the file
+    fs::write(GAME_PATH_STORAGE, remaining_games.join("\n"))
+        .map_err(|e| format!("Failed to write to file: {}", e))?;
+
+    // Return a success message
+    Ok(format!("Game {} removed", game_name))
+}
+
+// A function that attempts to execute the file at the given path
 #[tauri::command]
 fn execute_file(file_path: String) -> Result<String, String> {
-    println!("Trying to execute file: {}", &file_path); // log file path
+    // Attempt to execute the file
     match Command::new(file_path.clone())
         .output() {
+        // If the file executed successfully
         Ok(output) => {
-            println!("Output: {:?}", output); // log command output
+            // If the program exited with a non zero status code return an error
             if !output.status.success() {
-                Err(format!("Failed to execute file: {:?}", output)) // provide command output in error
+                Err(format!("Failed to execute file: {:?}", output)) 
             } else {
-                Ok(format!("Successfully executed file: {:?}", output)) // provide command output in success
+                // otherwise return a success message
+                Ok(format!("Successfully executed file: {:?}", output)) 
             }
         },
+        // If the file failed to execute  return an error
         Err(err) => Err(err.to_string())
     }
 }
-// fn execute_file(file_path: String) -> Result<(), String> {
-//     let output = Command::new(file_path)
-//         .output()
-//         .map_err(|err| err.to_string())?;
 
-//     if !output.status.success() {
-//         return Err("Failed to execute file".to_string());
-//     }
-
-//     Ok(())
-// }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, execute_file,get_file_path])
+        // commands
+        .invoke_handler(tauri::generate_handler![get_games, remove_game, execute_file,get_file_path])
+        // run application.
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
@@ -8,63 +8,76 @@ function App() {
   const [name, setName] = useState("");
   const [file, setFile] = useState("");
   const [err, setErr] = useState("");
+  const [files, setFiles] = useState([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  // Fetches the games and sets the state
+  const fetchGames = async () => {
+    try {
+      const gameData = await invoke("get_games");
+      const fileLines = gameData.split("\n").filter(Boolean);
+      setFiles(fileLines);
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
 
   async function openFileDialog() {
     try {
-      const filePath = await invoke("get_file_path");
-      setFile(filePath);
+      await invoke("get_file_path");
+      fetchGames(); // Fetch games after adding
     } catch (err) {
       console.error(err);
     }
   }
 
-  async function executeFile() {
-    setErr("here");
+  async function removeGame(gameName) {
     try {
-      await invoke("execute_file", { filePath: file });
+      await invoke("remove_game", { gameName });
+      fetchGames(); // Fetch games after removing
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function executeFile(gamePath) {
+    try {
+      await invoke("execute_file", { filePath: gamePath });
     } catch (err) {
       console.error(err);
       setErr(err);
     }
   }
+
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="title">
+        <div>
+          <h1>Favorite Games Launcher(made with rust)</h1>
+        </div>
+        <div>
+          <button onClick={openFileDialog}>Add Game</button>
+        </div>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <button onClick={() => openFileDialog()}>Open Tfauri Dialog</button>
-      <p>{greetMsg}</p>
-      <p>{file}</p> <button onClick={() => executeFile()}>Execute file</button>
-      <p>{err}</p>
+
+      <h2>Game List</h2>
+      {files.map((game, index) => {
+        const gamePathParts = game.split("\\");
+        const gameName =
+          gamePathParts[gamePathParts.length - 1].split(".exe")[0];
+
+        return (
+          <div key={index}>
+            <p>{game}</p>
+            <button onClick={() => executeFile(game)}>Play</button>
+            <button onClick={() => removeGame(game)}>Remove</button>
+          </div>
+        );
+      })}
+      {err}
     </div>
   );
 }
