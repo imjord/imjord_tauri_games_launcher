0364ef18ab71704849c5e1cd21e72b9a7f6f254c
imjord <dean.jay.goat@gmail.com>
2023-07-17 20:06:10 -0600

    added rust documentation for rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use rfd::FileDialog;
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 use std::process::Command;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
 
-// Define the name of the text file that will store the paths of the games
+/// This file path is for storing the game paths chosen by the user.
+/// 
+/// constant represents file path for txt file called "game_paths.txt".
+/// The file is expected to be made in the same directory along side the app.
+/// The contents of this file used to store the paths related to the exe chosen by the user.
 const GAME_PATH_STORAGE: &str = "./game_paths.txt";
+/// This file path is for the disclaimer created due to user click.
+/// 
+/// Constant represents file path for the txt file called "settings.txt".
+/// The file is expected to be created in the same directory along side the app.
+/// The contents of this file will be empty. it is only created once a user clicks "dont show again" on the app.
+/// If this file is there the disclaimer wont show in app. if this file is absent the disclaimer will show.
 const DISCLAIMER_SETTINGS: &str = "./settings.txt";
 
-// menu stuff
-// let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-// let close = CustomMenuItem::new("close".to_string(), "Close");
-// let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-// let menu = Menu::new()
-//   .add_native_item(MenuItem::Copy)
-//   .add_item(CustomMenuItem::new("hide", "Hide"))
-//   .add_submenu(submenu);
 
 
+
+/// Function that Creates the disclaimer settings file.
+/// 
+/// This function is invoked as a Tauri command using #[tauri::command].
+/// It creates a settings file named "settings.txt" in the current directory.
+/// 
+/// # Panics
+/// 
+/// This function will panic if it fails to create the file. With the following error Failed to create settings file
 #[tauri::command]
 fn disclaimer() {
- // create disclaimer file
  OpenOptions::new()
  .write(true)
  .create(true)
@@ -31,7 +40,14 @@ fn disclaimer() {
  .expect("Failed to create settings file");
 }
 
-
+/// Function that "checks" to see if the settings file is in the root directory returns string.
+/// 
+/// This function is invoked as a Tauri command using #[tauri::command].
+/// It creates a settings file named [settings.txt](constant@crate::DISCLAIMER_SETTINGS) in the current directory, which will be used to store disclaimer settings.
+/// 
+/// # Panics
+/// 
+/// This function will panic if the file is not in the root directory with the following error "Failed to read from file: "error"."
 #[tauri::command]
 fn get_disclaimer() -> Result<String, String> {
     let contents = fs::read_to_string(DISCLAIMER_SETTINGS)
@@ -40,24 +56,37 @@ fn get_disclaimer() -> Result<String, String> {
 }
 
 
-// A function that reads the game paths from the text file and returns them as a string
+/// Function that reads the game paths from the text file and returns them as a string.
+/// 
+/// This function is invoked as a Tauri command using #[tauri::command].
+/// It reads the contents of [game_paths.txt](constant@crate::GAME_PATH_STORAGE) in the current directory, which will be returned as string.
+///
+/// # Panics
+/// 
+/// This function will panic if it cant read the file with the following error "Failed to read from file: "error""
 #[tauri::command]
 fn get_games() -> Result<String, String> {
-    // Attempt to read the contents of the game paths file
     let contents = fs::read_to_string(GAME_PATH_STORAGE)
         .map_err(|e| format!("Failed to read from file: {}", e))?;
-    // Return the contents of the file
     Ok(contents)
 }
 
-// A function that opens a file dialog for the user to select a game executable,
-// then adds the path of that executable to the game paths file
+/// A function that opens a file dialog for the user to select a game executable returns a string.
+/// 
+/// /// This function is invoked as a Tauri command using #[tauri::command].
+/// This function filters .exe files only.
+/// Dialog will show for the user to pick a exe.
+/// Picked file path will be converted to a string.
+/// file path will be appended to the [game_paths.txt](constant@crate::GAME_PATH_STORAGE) txt file.
+/// if no file is selected it returns none.
+/// 
+/// # Panics
+/// 
+/// This function will panic if it fails to convert path to string, fails to open the file to append the path, fails to write to the game_paths file.
 #[tauri::command]
 fn get_file_path() -> Result<String, String> {
-    // Create a new file dialog that filters for .exe files only
     let dialog = FileDialog::new().add_filter("Exe Files", &["exe"]);
 
-    // Wait for the user to pick a file
     match dialog.pick_file() {
         // If the user picked a file
         Some(file_path) => {
@@ -84,7 +113,16 @@ fn get_file_path() -> Result<String, String> {
     }
 }
 
-// A function that removes a game from the game paths file.
+/// A function that removes a game from the game paths file and returns a Result<String, String>.
+/// 
+/// This function is invoked as a Tauri command using #[tauri::command].
+/// This function will attempt to read the contents of [game_paths.txt](constant@crate::GAME_PATH_STORAGE).
+/// This function will filter the lines that matches the math and remove. 
+/// The paths not removed will write back to the file. 
+/// 
+/// # Panics
+/// 
+/// This function will panic if it fails to read from file, or fails to write the paths back.
 #[tauri::command]
 fn remove_game(game_name: String) -> Result<String, String> {
     // Attempt to read the contents of the game paths file
@@ -108,7 +146,14 @@ fn remove_game(game_name: String) -> Result<String, String> {
     Ok(format!("Game {} removed", game_name))
 }
 
-// A function that attempts to execute the file at the given path
+/// A function that attempts to execute the file at the given path
+/// 
+/// This function is invoked as a Tauri command using #[tauri::command].
+/// This function is used to execute the content/paths from [game_paths.txt](constant@crate::GAME_PATH_STORAGE).
+/// 
+/// # Panics
+/// 
+/// This function will panic if it fails to execute the path files!
 #[tauri::command]
 fn execute_file(file_path: String) -> Result<String, String> {
     // Attempt to execute the file
@@ -129,6 +174,14 @@ fn execute_file(file_path: String) -> Result<String, String> {
     }
 }
 
+
+/// Main.
+///
+/// Will create the file [game_paths.txt](constant@crate::GAME_PATH_STORAGE) to main directory if it doesnt exist already,
+/// 
+/// # Panics
+///
+/// This function will panic if it fails to create the file specified by the constant [game_paths.txt](constant@crate::GAME_PATH_STORAGE).
 
 fn main() {
     // Create the file if it does not exist on main launch
